41df04db8e5a3d0a23442a87997b61df51fac952
thiagofarias-dev <thiagofarias13@hotmail.com>
2021-06-08 19:26:05 -0300

    Criando classe Conta

--- teste.py
+++ teste.py
@@ -1,14 +1,15 @@
-
-
 def criar_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
+
 def deposita(conta, valor):
     conta["saldo"] += valor
 
+
 def saque(conta, valor):
-        conta["saldo"] -= valor
+    conta["saldo"] -= valor
+
 
 def extrato(conta):
     print("Saldo é {}".format(conta["saldo"]))
