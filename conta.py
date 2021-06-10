3f9f4e8ead1b4b6f85f06c99b18c21b1af9596b5
thiagofarias-dev <thiagofarias13@hotmail.com>
2021-06-10 20:38:09 -0300

    Criação classe Data. Ajustes classe Conta

--- conta.py
+++ conta.py
@@ -9,3 +9,11 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print("Saldo de R${} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
---
+++ datas.py
@@ -0,0 +1,11 @@
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatada(self):
+        print(f'{self.dia:02d}/{self.mes:02d}/{self.ano}')
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
+
